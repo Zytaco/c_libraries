8158fd53b6c0790e35ebbc7f3d72a177c7e16eb1
Jim Heeresma <jheeresm@f1r5s11.codam.nl>
2019-11-07 13:23:43 +0100

    init

---
+++ libsort/includes/libsort.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   libsort.h                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jheeresm <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/04 17:50:03 by jheeresm      #+#    #+#                 */
+/*   Updated: 2019/11/04 17:50:03 by jheeresm      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBSORT_H
+# define LIBSORT_H
+
+typedef struct		s_array
+{
+	int 			size;
+	int				*array;
+}					t_array;
+
+#endif
